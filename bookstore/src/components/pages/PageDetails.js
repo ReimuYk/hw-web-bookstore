2ed30b8a743eb55f31af847730a5d3636ade0361
yuki.h <yuki.h@sjtu.edu.cn>
2018-03-31 23:12:17 +0800

    banner

--- bookstore/src/components/pages/PageDetails.js
+++ bookstore/src/components/pages/PageDetails.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { Col, Row} from 'antd'
+import { Col, Row, Button} from 'antd'
 
 class PageDetails extends React.Component{
     constructor(props) {
@@ -12,12 +12,33 @@ class PageDetails extends React.Component{
             {/* <p>this is book details {this.props.location.search.substring(5)}</p> */}
             <Row type="flex" justify="center" align="top">
                 <Col style={{width:200,margin:80}} span={6}>
-                    <img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" style={{width:200}}/>
+                    <img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" style={{width:250}}/>
                 </Col>
                 <Col span={12} style={{align:'left',marginTop:80,width:400}}>
-                    <h1 style={{fontSize:30,align:'left'}}>ID:{this.props.location.search.substring(5)}</h1>
-                    <h1 style={{fontSize:30,align:'left'}}>书名:这是一本书</h1>
-                    <h1 style={{fontSize:30,align:'left'}}>简介:这是一段简介</h1>
+                    <ul>
+                        <li style={{fontSize:30}}>
+                            ID:{this.props.location.search.substring(5)}
+                        </li>
+                        <li style={{fontSize:30}}>
+                            书名：这是一个书名
+                        </li>
+                        <li style={{fontSize:30}}>
+                            出版社：这是一个出版社
+                        </li>
+                        <li style={{fontSize:30}}>
+                            出版年月：这是一个出版年月
+                        </li>
+                        <li style={{fontSize:30}}>
+                            简介：这是一段简介
+                        </li>
+                        <li style={{fontSize:30}}>
+                            价格：99.98¥
+                        </li>
+                        <li style={{fontSize:30}}>
+                            <Button type="primary" style={{margin:20}}>加入购物车</Button>
+                            <Button type="primary" style={{margin:20}}>立即购买</Button>
+                        </li>
+                    </ul>
                 </Col>
             </Row>
             </div>
