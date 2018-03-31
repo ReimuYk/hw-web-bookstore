e51deab17851b05f26c9c1b407e494ef6b21cc12
yuki.h <yuki.h@sjtu.edu.cn>
2018-03-31 17:32:42 +0800

    收藏夹

--- bookstore/src/components/pages/HomepageFav.js
+++ bookstore/src/components/pages/HomepageFav.js
@@ -1,9 +1,58 @@
 import React, {Component} from 'react';
+import { List, Card } from 'antd';
+const { Meta} = Card;
+
+const data = [
+  {
+    title: 'book1',
+    description: 'description of book1'
+  },
+  {
+    title: 'book2',
+    description: 'description of book2'
+  },
+  {
+    title: 'book3',
+    description: 'description of book3'
+  },
+  {
+    title: 'book4',
+    description: 'description of book4'
+  },
+  {
+    title: 'book5',
+    description: 'description of book5'
+  },
+  {
+    title: 'book6',
+    description: 'description of book6'
+  }
+    
+];
 
 class Favorite extends Component{
     render(){
         return (
-            <p>this is favorite</p>
+            <div style={{width:1300,margin:'auto'}}>
+            <List
+            grid={{ gutter: 16, column: 4 }}
+            dataSource={data}
+            renderItem={item => (
+              <List.Item>
+                <Card
+                    hoverable
+                    style={{ width: 240 }}
+                    cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png" />}
+                >
+                    <Meta
+                    title={item.title}
+                    description={item.description}
+                    />
+                </Card>
+              </List.Item>
+                )}
+            />
+            </div>
         )
     }
 }
